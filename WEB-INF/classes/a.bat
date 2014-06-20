a9504e91c69cac10c7e8cb8a9d9365f21d4b8004
Srinivas <srinivas.jonnadula@gmail.com>
2014-06-20 22:06:44 +0530

    Commit all the changes

---
+++ WEB-INF/classes/a.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=C:\jdk1.5
+set path=C:\jdk1.5\bin
+set classpath=C:\jdk1.5\lib\tools.jar;C:\ServletEx\Ex1\WEB-INF\lib\servlet.jar
---
+++ readme.txt
@@ -0,0 +1,4 @@
+HelloWorld.java
+http://localhost:8080/Ex1/hello
+Output:
+	Welcome to servlets
